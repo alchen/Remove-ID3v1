03d15e7ac9b68b80f9d3a57ce6777287cd355984
Alexander Chen <alex7c@gmail.com>
2018-01-21 23:48:27 +0900

    Update jasmine spec to match updated library structure

--- spec/spec.js
+++ spec/spec.js
@@ -1,22 +1,25 @@
-describe("Remove ID3v1", function() {
-  const Remover = require('../src/remove-id3v1');
-  const fs = require('fs');
-  const path = require('path');
-  const assetFolder = path.join(__dirname, 'assets');
+const Remover = require('../src/remove-id3v1');
+const fs = require('fs');
+const path = require('path');
 
-  it("should be able to remove ID3v1 tags when present", function() {
-    let songBuffer = fs.readFileSync(path.join(assetFolder, 'sample_id3v1.mp3'));
+const assetFolder = path.join(__dirname, 'assets');
+
+describe('Remove ID3v1', () => {
+  it('should be able to remove ID3v1 tags when present', () => {
+    const songBuffer = fs.readFileSync(path.join(assetFolder, 'sample_id3v1.mp3'));
+    const newBuffer = Remover.removeTag(songBuffer);
+
     expect(songBuffer.byteLength).toEqual(83300);
-
-    const remover = new Remover(songBuffer);
-    expect(remover.arrayBuffer.byteLength).toEqual(83172);
+    expect(newBuffer.byteLength).toEqual(83172);
+    expect(songBuffer).not.toBe(newBuffer);
   });
 
-  it("should do nothing when ID3v1 tag is not present", function() {
-    let songBuffer = fs.readFileSync(path.join(assetFolder, 'sample_tagless.mp3'));
-    expect(songBuffer.byteLength).toEqual(83172);
+  it('should do nothing when ID3v1 tag is not present', () => {
+    const songBuffer = fs.readFileSync(path.join(assetFolder, 'sample_tagless.mp3'));
+    const newBuffer = Remover.removeTag(songBuffer);
 
-    const remover = new Remover(songBuffer);
-    expect(remover.arrayBuffer.byteLength).toEqual(83172);
+    expect(songBuffer.byteLength).toEqual(83172);
+    expect(newBuffer.byteLength).toEqual(83172);
+    expect(songBuffer).not.toBe(newBuffer);
   });
 });
